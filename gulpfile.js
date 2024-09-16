e8d5f9a10a5f11d9e9e761b6cf8f210fda5524bb
Artjoms Bugajenkovs <k3nnel123@gmail.com>
2024-09-16 15:00:52 +0300

    edit: added configs and adjusted architecture

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable no-param-reassign */
+/* eslint-disable consistent-return */
+
 const { src, dest, watch, parallel, series } = require('gulp');
 
 // HTML
@@ -8,8 +12,8 @@ const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const sassGlob = require('gulp-sass-glob');
 // JavaScript
-const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const rename = require('gulp-rename');
 // Fonts
@@ -21,109 +25,170 @@ const newer = require('gulp-newer');
 const browserSync = require('browser-sync').create();
 // Utility
 const changed = require('gulp-changed');
-const clean = require('gulp-clean')
-const fs = require('fs')
+const clean = require('gulp-clean');
+const fs = require('fs');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
 
+// const srcPath = 'src';
+// const distPath = 'dist';
 
-// Deleting dist folder 
-function cleanDist(done) {
-    // check if the folder exists
-    if (fs.existsSync('./dist')) {
-        return src('./dist', { read: false })
-        .pipe(clean({ force: true }))
-    }
-    // if doesn't use callback
-    done();
+// const paths = {
+//   src: {
+//     root: srcPath,
+//     html: srcPath,
+//     fonts: `${srcPath}/assets/fonts/accomodate/**/*.woff2`,
+//     images: `${srcPath}/assets/images/accomodate/**/*`,
+//     js: `${srcPath}/js/accomodate/**/*.js`,
+//     scss: `${srcPath}/scss/**/*.scss`,
+//     pages: `${srcPath}/pages/**/*.html`,
+//   },
+
+//   dist: {
+//     root: distPath,
+//     html: distPath,
+//     css: `${distPath}/css`,
+//     js: `${distPath}/js`,
+//     fonts: `${distPath}/fonts`,
+//     images: `${distPath}/images`,
+//   },
+
+//   watch: {
+//     root: srcPath,
+//     html: srcPath,
+//     fonts: `${srcPath}/assets/fonts/accomodate/**/*.woff2`,
+//     images: `${srcPath}/assets/images/accomodate/**/*`,
+//     js: `${srcPath}/js/accomodate/**/*.js`,
+//     scss: `${srcPath}/scss/**/*.scss`,
+//     pages: `${srcPath}/pages/**/*.html`,
+//   },
+
+//   clean: `./${distPath}`,
+// };
+
+// Deleting dist folder
+function del(done) {
+  // Check if the folder already exists
+  if (fs.existsSync('./dist' && './src/css')) {
+    return src('./dist' && './src/css', { read: false }).pipe(clean({ force: true }));
+  }
+  // If it doesn't use the callback
+  done();
 }
 
-// Taks for fonts
+// Plumber notify function
+function plumberNotify() {
+  notify.onError({
+    title: 'title',
+    message: 'Error <%= error.message %>',
+    sound: false,
+  });
+}
+
+// Taks for fonts converting
 function fonts() {
-    return src('./src/fonts/accomodate/**/*.*')
-        .pipe(changed('./src/fonts', { extension: '.woff2' }))
-        .pipe(ttf2woff2())
-        .pipe(dest('./src/fonts'))
+  return src('./src/assets/fonts/accomodate/**/*.*')
+    .pipe(changed('./src/assets/fonts/accomodate', { extension: '.woff2' }))
+    .pipe(ttf2woff2())
+    .pipe(dest('./src/assets/fonts'));
 }
 
-// Task for images
+// Task for images converting
 function images() {
-    return src('./src/img/accomodate/**/*.*')
-        .pipe(newer('./src/img'))
-        .pipe(webp())
-        .pipe(dest('./src/img'))
+  return src('./src/assets/img/accomodate/**/*.*')
+    .pipe(newer('./src/assets/img'))
+    .pipe(webp())
+    .pipe(dest('./src/assets/img'));
 }
 
 // Task for JS files
 function scripts() {
-    return src('./src/js/accomodate/**/*.js')
-        .pipe(rigger())
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".js"
-        }))
-        .pipe(dest('./src/js'))
-        .pipe(browserSync.stream());
+  return src('./src/js/accomodate/**/*.js')
+    .pipe(plumber(plumberNotify('JS')))
+    .pipe(rigger())
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: '.min',
+        extname: '.js',
+      }),
+    )
+    .pipe(dest('./src/js'))
+    .pipe(browserSync.stream());
 }
 
 // Task for HTML files
 function pages() {
-    return src('./src/**/*.dev.html', {base: './src/pages'})
-        .pipe(include({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(rename(function (path) {
-            path.basename = path.basename.replace(".dev", "");
-            path.extname = ".html";
-        }))
-        .pipe(dest('src')) 
-        .pipe(browserSync.stream());
+  return src('./src/**/*.dev.html', { base: './src/pages' })
+    .pipe(plumber(plumberNotify('HTML')))
+    .pipe(
+      include({
+        prefix: '@@',
+        basepath: '@file',
+      }),
+    )
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(
+      rename((path) => {
+        path.basename = path.basename.replace('.dev', '');
+        path.extname = '.html';
+      }),
+    )
+    .pipe(dest('./src'))
+    .pipe(browserSync.stream());
 }
 
 // Task for SASS
 function styles() {
-    return src('./src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest("./src/css"))
-        .pipe(browserSync.stream())
+  return src('./src/scss/**/*.scss')
+    .pipe(plumber(plumberNotify('SCSS')))
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(
+      rename({
+        suffix: '.min',
+      }),
+    )
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 5 version'] }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('./src/css'))
+    .pipe(browserSync.stream());
 }
 
-// Watch for changes
+// Watch for the changes
 function watching() {
-    browserSync.init({
-        server: {
-            baseDir: "./src/"
-        }
-    });
-    watch(['./src/fonts/accomodate/**/*.*'], fonts)
-    watch(['./src/img/accomodate/**/*.*'], images)
-    watch(['./src/partials/**/*.html', './src/**/*.dev.html'], pages)
-    watch(['./src/scss/**/*.scss'], styles)
-    watch(['./src/js/accomodate/**/*.js', './src/js/components/**/*.js'], scripts)
-        .on('change', browserSync.reload)
+  browserSync.init({
+    server: {
+      baseDir: './src',
+    },
+  });
+  watch(['./src/assets/fonts/accomodate/**/*.*'], fonts);
+  watch(['./src/assets/img/accomodate/**/*.*'], images);
+  watch(['./src/pages/**/*.html', './src/pages/**/*.dev.html'], pages);
+  watch(['./src/scss/**/*.scss'], styles);
+  watch(
+    ['./src/js/accomodate/**/*.js', './src/js/components/**/*.js'],
+    scripts,
+  ).on('change', browserSync.reload);
 }
 
-// Build project
+// Build the project - create dist folder
 function building() {
-    return src([
-        './src/img/**/*.*',
-        '!./src/img/accomodate/**/*.*',
-        './src/fonts/**/*.*',
-        '!./src/fonts/accomodate/**/*.*',
-        './src/*.html',
-        '!./src/pages/*.dev.html',
-        '!./src/pages/**/*.*',
-        './src/css/*.css',
-        './src/js/**/*.min.js',
-    ], {base : './src'})
-        .pipe(dest('./dist'))
+  return src(
+    [
+      './src/assets/img/**/*.*',
+      '!./src/assets/img/accomodate/**/*.*',
+      './src/assets/fonts/**/*.*',
+      '!./src/assets/fonts/accomodate/**/*.*',
+      './src/*.html',
+      '!./src/pages/*.dev.html',
+      '!./src/pages/**/*.*',
+      './src/css/*.css',
+      './src/js/**/*.min.js',
+    ],
+    { base: './src' },
+  ).pipe(dest('./dist'));
 }
 
 exports.fonts = fonts;
@@ -133,7 +198,7 @@ exports.pages = pages;
 exports.styles = styles;
 exports.building = building;
 exports.watching = watching;
-exports.cleanDist = cleanDist;
+exports.del = del;
 
-exports.build = series(cleanDist, fonts, images, scripts, pages, styles, building);
+exports.build = series(del, fonts, images, scripts, pages, styles, building);
 exports.default = parallel(fonts, images, scripts, pages, styles, watching);
