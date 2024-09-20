35887a9428b3da7c457072875876a469ca73c917
Artjoms Bugajenkovs <k3nnel123@gmail.com>
2024-09-20 21:57:52 +0300

    added:husky hooks, eslint, lint-staged, prettier configs

--- gulpfile.js
+++ gulpfile.js
@@ -66,132 +66,132 @@ const notify = require('gulp-notify');
 // Deleting dist folder
 // eslint-disable-next-line consistent-return
 function del(done) {
-	// Check if the folder already exists
-	if (fs.existsSync('./dist' && './src/css')) {
-		return src('./dist' && './src/css', { read: false }).pipe(
-			clean({ force: true })
-		);
-	}
-	// If it doesn't use the callback
-	done();
+  // Check if the folder already exists
+  if (fs.existsSync('./dist' && './src/css')) {
+    return src('./dist' && './src/css', { read: false }).pipe(
+      clean({ force: true })
+    );
+  }
+  // If it doesn't use the callback
+  done();
 }
 
 // Plumber notify function
 function plumberNotify() {
-	notify.onError({
-		title: 'title',
-		message: 'Error <%= error.message %>',
-		sound: false
-	});
+  notify.onError({
+    title: 'title',
+    message: 'Error <%= error.message %>',
+    sound: false
+  });
 }
 
 // Taks for fonts converting
 function fonts() {
-	return src('./src/assets/fonts/accomodate/**/*.*')
-		.pipe(changed('./src/assets/fonts/accomodate', { extension: '.woff2' }))
-		.pipe(ttf2woff2())
-		.pipe(dest('./src/assets/fonts'));
+  return src('./src/assets/fonts/accomodate/**/*.*')
+    .pipe(changed('./src/assets/fonts/accomodate', { extension: '.woff2' }))
+    .pipe(ttf2woff2())
+    .pipe(dest('./src/assets/fonts'));
 }
 
 // Task for images converting
 function images() {
-	return src('./src/assets/img/accomodate/**/*.*')
-		.pipe(newer('./src/assets/img'))
-		.pipe(webp())
-		.pipe(dest('./src/assets/img'));
+  return src('./src/assets/img/accomodate/**/*.*')
+    .pipe(newer('./src/assets/img'))
+    .pipe(webp())
+    .pipe(dest('./src/assets/img'));
 }
 
 // Task for JS files
 function scripts() {
-	return src('./src/js/accomodate/**/*.js')
-		.pipe(plumber(plumberNotify('JS')))
-		.pipe(rigger())
-		.pipe(uglify())
-		.pipe(
-			rename({
-				suffix: '.min',
-				extname: '.js'
-			})
-		)
-		.pipe(dest('./src/js'))
-		.pipe(browserSync.stream());
+  return src('./src/js/accomodate/**/*.js')
+    .pipe(plumber(plumberNotify('JS')))
+    .pipe(rigger())
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: '.min',
+        extname: '.js'
+      })
+    )
+    .pipe(dest('./src/js'))
+    .pipe(browserSync.stream());
 }
 
 // Task for HTML files
 function pages() {
-	return src('./src/**/*.dev.html', { base: './src/pages' })
-		.pipe(plumber(plumberNotify('HTML')))
-		.pipe(
-			include({
-				prefix: '@@',
-				basepath: '@file'
-			})
-		)
-		.pipe(htmlmin({ collapseWhitespace: true }))
-		.pipe(
-			rename((path) => {
-				// eslint-disable-next-line no-param-reassign
-				path.basename = path.basename.replace('.dev', '');
-				// eslint-disable-next-line no-param-reassign
-				path.extname = '.html';
-			})
-		)
-		.pipe(dest('./src'))
-		.pipe(browserSync.stream());
+  return src('./src/**/*.dev.html', { base: './src/pages' })
+    .pipe(plumber(plumberNotify('HTML')))
+    .pipe(
+      include({
+        prefix: '@@',
+        basepath: '@file'
+      })
+    )
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(
+      rename((path) => {
+        // eslint-disable-next-line no-param-reassign
+        path.basename = path.basename.replace('.dev', '');
+        // eslint-disable-next-line no-param-reassign
+        path.extname = '.html';
+      })
+    )
+    .pipe(dest('./src'))
+    .pipe(browserSync.stream());
 }
 
 // Task for SASS
 function styles() {
-	return src('./src/scss/**/*.scss')
-		.pipe(plumber(plumberNotify('SCSS')))
-		.pipe(sourcemaps.init())
-		.pipe(sassGlob())
-		.pipe(sass({ outputStyle: 'compressed' }))
-		.pipe(
-			rename({
-				suffix: '.min'
-			})
-		)
-		.pipe(autoprefixer({ overrideBrowserslist: ['last 5 version'] }))
-		.pipe(sourcemaps.write('.'))
-		.pipe(dest('./src/css'))
-		.pipe(browserSync.stream());
+  return src('./src/scss/**/*.scss')
+    .pipe(plumber(plumberNotify('SCSS')))
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 5 version'] }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('./src/css'))
+    .pipe(browserSync.stream());
 }
 
 // Watch for the changes
 function watching() {
-	browserSync.init({
-		server: {
-			baseDir: './src'
-		}
-	});
+  browserSync.init({
+    server: {
+      baseDir: './src'
+    }
+  });
 
-	watch(['./src/assets/fonts/accomodate/**/*.*'], fonts);
-	watch(['./src/assets/img/accomodate/**/*.*'], images);
-	watch(['./src/pages/**/*.html', './src/pages/**/*.dev.html'], pages);
-	watch(['./src/scss/**/*.scss'], styles);
-	watch(
-		['./src/js/accomodate/**/*.js', './src/js/components/**/*.js'],
-		scripts
-	).on('change', browserSync.reload);
+  watch(['./src/assets/fonts/accomodate/**/*.*'], fonts);
+  watch(['./src/assets/img/accomodate/**/*.*'], images);
+  watch(['./src/pages/**/*.html', './src/pages/**/*.dev.html'], pages);
+  watch(['./src/scss/**/*.scss'], styles);
+  watch(
+    ['./src/js/accomodate/**/*.js', './src/js/components/**/*.js'],
+    scripts
+  ).on('change', browserSync.reload);
 }
 
 // Build the project - create dist folder
 function building() {
-	return src(
-		[
-			'./src/assets/img/**/*.*',
-			'!./src/assets/img/accomodate/**/*.*',
-			'./src/assets/fonts/**/*.*',
-			'!./src/assets/fonts/accomodate/**/*.*',
-			'./src/*.html',
-			'!./src/pages/*.dev.html',
-			'!./src/pages/**/*.*',
-			'./src/css/*.css',
-			'./src/js/**/*.min.js'
-		],
-		{ base: './src' }
-	).pipe(dest('./dist'));
+  return src(
+    [
+      './src/assets/img/**/*.*',
+      '!./src/assets/img/accomodate/**/*.*',
+      './src/assets/fonts/**/*.*',
+      '!./src/assets/fonts/accomodate/**/*.*',
+      './src/*.html',
+      '!./src/pages/*.dev.html',
+      '!./src/pages/**/*.*',
+      './src/css/*.css',
+      './src/js/**/*.min.js'
+    ],
+    { base: './src' }
+  ).pipe(dest('./dist'));
 }
 
 exports.fonts = fonts;
--- src/js/accomodate/main.js
+++ src/js/accomodate/main.js
@@ -1,2 +1,4 @@
+/* eslint-disable no-console */
+
 //= ../components/modal.js
 console.log('Hello');
